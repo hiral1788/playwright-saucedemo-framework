e55da23d52565560955bb8ab6f6a3291b0494056
Hiralkumar Patel~ <hiral.de.damao@live.com>
2026-06-22 22:47:44 +0530

    Add playwright fixture and reference tests

--- tests/addToCart.spec.js
+++ tests/addToCart.spec.js
@@ -1,5 +1,5 @@
-import { test } from '@playwright/test';
-
+//import { test } from '@playwright/test';
+import { test } from '../fixtures/baseTest.js';
 import { LoginPage } from '../pages/LoginPage.js';
 import { InventoryPage } from '../pages/InventoryPage.js';
 import { CartPage } from '../pages/CartPage.js';
@@ -9,17 +9,21 @@ import { users } from '../data/users.js';
 for(const user of users){
 
     test(
-        `Login using ${user.username}`,
-        async ({ page }) => {
+  `Login using ${user.username}`,
+  async ({
+      loginPage,
+      inventoryPage,
+      cartPage
+  }) => {
 
-            const loginPage =
+            /*const loginPage =
                 new LoginPage(page);
 
             const inventoryPage =
                 new InventoryPage(page);
 
             const cartPage =
-                new CartPage(page);
+                new CartPage(page);*/
 
             await loginPage.navigate();
 
