37eb7617c16433c080607bb4c54856e54b6dcb98
Hiralkumar Patel~ <hiral.de.damao@live.com>
2026-07-02 23:06:06 +0530

    Added environment config for base URL

--- data/users.js
+++ data/users.js
@@ -1,18 +1,3 @@
-/*export const user = {
-    standard: {
-        username: 'standard_user',
-        password: 'secret_sauce',
-        canLogin: true
-    },
-
-    locked: {
-        username: 'locked_out_user',
-        password: 'secret_sauce',
-        canLogin: false
-    }
-};*/
-import { expect } from '@playwright/test';
-
 export const users = [
     {
         username: 'standard_user',
--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -1,43 +1,31 @@
-import { expect } from "@playwright/test";
+import { expect } from '@playwright/test';
+import { env } from '../config/env.js';
 
 export class LoginPage {
 
     constructor(page) {
         this.page = page;
 
-        this.username = '[data-test="username"]';
-        this.password = '[data-test="password"]';
-        this.loginBtn = '[data-test="login-button"]';
+        this.username = page.locator('[data-test="username"]');
+        this.password = page.locator('[data-test="password"]');
+        this.loginBtn = page.locator('[data-test="login-button"]');
     }
 
     async navigate() {
-        await this.page.goto('https://www.saucedemo.com/');
+        await this.page.goto(env.baseUrl);
     }
 
+    async login(username, password) {
+        await this.username.fill(username);
+        await this.password.fill(password);
+        await this.loginBtn.click();
+    }
 
-  
-
-async verifyLockedUserError(){
-
-    await expect(
-        this.page.locator('[data-test="error"]')
-    ).toContainText(
-        'Sorry, this user has been locked out.'
-    );
-}
-
-    async login(user, pass) {
-
-        await this.page
-            .locator(this.username)
-            .fill(user);
-
-        await this.page
-            .locator(this.password)
-            .fill(pass);
-
-        await this.page
-            .locator(this.loginBtn)
-            .click();
+    async verifyLockedUserError() {
+        await expect(
+            this.page.locator('[data-test="error"]')
+        ).toContainText(
+            'Sorry, this user has been locked out.'
+        );
     }
 }
